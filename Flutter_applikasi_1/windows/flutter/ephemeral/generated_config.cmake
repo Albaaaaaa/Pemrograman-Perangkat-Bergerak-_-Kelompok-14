295da17e6e7db901df11f16773ceb0a301204adf
Muhammad ulil Albab <114996165+Albaaaaaa@users.noreply.github.com>
2024-02-11 14:57:49 +0800

    Add files via upload

---
+++ Flutter_applikasi_1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Pemrograman Perangkat Bergerak\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Pemrograman Perangkat Bergerak\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Pemrograman Perangkat Bergerak\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mMzRjNjU4Yjk2MDBjMzgwYmMyNjA4ZmE1NjIyZjM1NDdmZDA1YzFjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS TUF\\Documents\\KULIAH\\SEMESTER 4\\Pemrograman Perangkat Bergerak\\flutter_application_1\\.dart_tool\\package_config.json"
+)
